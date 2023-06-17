e821deaf97d67deacaa060b4a1a514a79fb8d465
caioliveirap <caio191201@gmail.com>
2023-06-17 10:39:04 -0300

    feat: created donator crud

--- routes/donators.ts
+++ routes/donators.ts
@@ -4,10 +4,10 @@ const router = Router();
 
 router.get("/list", async (req, res) => {
   try {
-    const list = await Donator.find();
+    const list = await Donator.find().exec();
     res.status(200).json({ success: true, data: list });
   } catch (error: any) {
-    res.status(400).send({ error: new Error(error), success: false });
+    res.status(400).send({ error: error.message, success: false });
   }
 });
 
@@ -17,17 +17,17 @@ router.get("/list-by-id/:id", async (req, res) => {
     const list = await Donator.findById(id);
     res.status(200).json({ success: true, data: list });
   } catch (error: any) {
-    res.status(400).send({ error: new Error(error), success: false });
+    res.status(400).send({ error: error.message, success: false });
   }
 });
 
-router.post("/add-users", async (req, res) => {
+router.post("/create-new", async (req, res) => {
   try {
     const donator = await new Donator(req.body);
     const save = await donator.save(donator);
     res.status(200).json({ success: true, data: save });
   } catch (error: any) {
-    res.status(400).send({ error: new Error(error), success: false });
+    res.status(400).send({ error: error.message, success: false });
   }
 });
 
@@ -39,19 +39,31 @@ router.put("/update-donator/:id", async (req, res) => {
     const update = await Donator.findOneAndUpdate({ _id: id }, data);
     res.status(200).json({ success: true, data: update });
   } catch (error: any) {
-    res.status(400).send({ error: new Error(error), success: false });
+    res.status(400).send({ error: error.message, success: false });
   }
 });
 
 //PUT replaces only the necessary fields
-router.patch("/update-donator-field/:id", async (req, res) => {
+router.patch("/update-field/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const data = req.body;
     const update = await Donator.findOneAndUpdate({ _id: id }, { $set: data });
     res.status(200).json({ success: true, data: update });
   } catch (error: any) {
-    res.status(400).send({ error: new Error(error), success: false });
+    res.status(400).send({ error: error.message, success: false });
+  }
+});
+
+//DELETE replaces only the necessary fields
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const update = await Donator.findOneAndUpdate({ _id: id }, { $set: data });
+    res.status(200).json({ success: true, data: update });
+  } catch (error: any) {
+    res.status(400).send({ error: error.message, success: false });
   }
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
 require("dotenv").config();
-const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
 
-MongoClient.connect(process.env.MONGODB_URI, (err: any, client: any) => {
-  console.log("db connected");
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err: any) => console.log(err));
 const app = express();
 
 app.use(express.json({ limit: "100mb" }));

fbb8eec8c2035ce762573d73fea312dabd30b623
caioliveirap <caio191201@gmail.com>
2023-06-17 12:10:10 -0300

    feat: added sign up with firebase

--- routes/index.ts
+++ routes/index.ts
@@ -4,5 +4,6 @@ const router = Router();
 /* GET home page. */
 router.use("/users", require("./users"));
 router.use("/donators", require("./donators"));
+router.use("/auth", require("./auth"));
 
 module.exports = router;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express from "express";
 require("dotenv").config();
 const mongoose = require("mongoose");
+const admin = require("firebase-admin");
+const credentials = require("../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(credentials),
+});
 
 mongoose
   .connect(process.env.MONGODB_URI, {

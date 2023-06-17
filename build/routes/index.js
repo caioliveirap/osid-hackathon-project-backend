5d06e55c6358b7bfca0ac0f958178d3049325f34
caioliveirap <caio191201@gmail.com>
2023-06-17 10:13:44 -0300

    feat: removed build from git

--- build/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-/* GET home page. */
-router.use("/users", require("./users"));
-module.exports = router;
